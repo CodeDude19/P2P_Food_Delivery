65569539b946ac08f2fffd568afe6fda89739d2a
yasser <yasser.info.work@gmail.com>
2020-04-24 09:17:05 +0530

    Updates!

--- p2p/lib/screens/productDetail.dart
+++ p2p/lib/screens/productDetail.dart
@@ -9,10 +9,64 @@ class ProductDetail extends StatefulWidget {
 class _ProductDetailState extends State<ProductDetail> {
   @override
   Widget build(BuildContext context) {
+    backAndCart() {
+      return Container(
+        padding: EdgeInsets.only(top: 30, left: 10, right: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.arrow_back),
+              color: Colors.white,
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              iconSize: 30,
+            ),
+            IconButton(
+              icon: Icon(Icons.shopping_cart),
+              color: Colors.white,
+              onPressed: () {},
+              iconSize: 30,
+            )
+          ],
+        ),
+      );
+    }
+
+    namePrice(name, price) {
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(
+              name.length > 25 ? name.substring(0, 20) + "...." : name,
+              softWrap: true,
+              style: TextStyle(
+                fontSize: 28,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "\$ $price",
+              softWrap: true,
+              style: TextStyle(
+                fontSize: 22,
+                color: Colors.black,
+              ),
+            )
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
       body: Stack(
         children: <Widget>[
           ListView(
+            padding: EdgeInsets.only(top: 20, bottom: 100),
             children: <Widget>[
               CachedNetworkImage(
                 width: MediaQuery.of(context).size.width,
@@ -21,29 +75,83 @@ class _ProductDetailState extends State<ProductDetail> {
                 imageUrl:
                     "https://images.unsplash.com/photo-1565299507177-b0ac66763828?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1928&q=80",
               ),
+              SizedBox(height: 20),
+              namePrice("Burger", "200"),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "Usha Chauhan",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      "\"A hamburger (also burger for short) is a sandwich consisting of one or more cooked patties of ground meat, usually beef, placed inside a sliced bread roll or bun. The patty may be pan fried, grilled, smoked or flame broiled. ... A hamburger topped with cheese is called a cheeseburger.\"",
+                      textAlign: TextAlign.left,
+                      style:
+                          TextStyle(fontSize: 17, fontStyle: FontStyle.italic),
+                    )
+                  ],
+                ),
+              )
             ],
           ),
-          Container(
-            padding: EdgeInsets.only(top: 30, left: 10, right: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                IconButton(
-                  icon: Icon(Icons.arrow_back),
-                  color: Colors.white,
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  iconSize: 30,
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              backAndCart(),
+              Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Expanded(
+                      child: FlatButton(
+                        padding: EdgeInsets.all(10),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(7),
+                        ),
+                        onPressed: () {},
+                        child: Text(
+                          "Add to Cart",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                        color: Colors.green,
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: FlatButton(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(7),
+                        ),
+                        padding: EdgeInsets.all(10),
+                        onPressed: () {},
+                        child: Text(
+                          "Favourite Chef",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                        color: Colors.redAccent,
+                      ),
+                    ),
+                  ],
                 ),
-                IconButton(
-                  icon: Icon(Icons.shopping_cart),
-                  color: Colors.white,
-                  onPressed: () {},
-                  iconSize: 30,
-                )
-              ],
-            ),
+              )
+            ],
           ),
         ],
       ),
